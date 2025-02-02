17b2ae12ab0b0ab277fe2c340be825ddf057d8c6
Sovannchhaktra <sovannchhaktra.nai@igtech.asia>
2025-02-02 15:45:24 +0700

    build screen otp 50%

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import router from "./router";
 import store from "./store/store";
 import "./style.css";
 
+import PrimeVue from "primevue/config";
+
 const app = createApp(App);
 
 // Use the router
 app.use(router);
 // Use the vuex
 app.use(store);
+app.use(PrimeVue);
 
 app.mount("#app");
--- src/views/SignIn/script.js
+++ src/views/SignIn/script.js
@@ -1,8 +1,24 @@
+import Button from "primevue/button";
 export default {
   name: "SignIn",
+
   data() {
     return {
       isPassword: true,
+      isNext: false,
+      title: "Persional",
     };
+  },
+  components: { Button },
+
+  updated() {
+    this.updateTitle();
+  },
+  methods: {
+    updateTitle() {
+      if (this.isNext) {
+        this.title = "Please Enter OTP";
+      }
+    },
   },
 };
