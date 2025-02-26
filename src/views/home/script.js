b817ec839e24a9fa5c4121dba0081478c792a004
Sovannchhaktra <sovannchhaktra.nai@igtech.asia>
2025-02-26 19:52:17 +0700

    Fix issues home

--- src/views/home/script.js
+++ src/views/home/script.js
@@ -2,30 +2,9 @@ export default {
   name: "Home",
   data() {
     return {
+      star: 5,
+      lists: 4,
       myData: "Hello world",
     };
-  },
-
-  // lifecycle hooks
-  beforeCreate() {
-    console.log("beforeCreate called");
-  },
-  created() {
-    console.log("created called");
-  },
-  beforeMount() {
-    console.log("beforeMount called");
-  },
-  mounted() {
-    console.log("mounted called");
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate called");
-  },
-  updated() {
-    console.log("updated called");
-  },
-  methods: {
-    onSave() {},
   },
 };
