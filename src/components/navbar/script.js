6b64171379af8941d318568330b02ac3dc39101a
Sovannchhaktra <sovannchhaktra.nai@igtech.asia>
2025-03-06 17:16:15 +0700

    enhancement translate

--- src/components/navbar/script.js
+++ src/components/navbar/script.js
@@ -1,13 +1,35 @@
 import MenuJson from "./menus.json";
+import { useI18n } from 'vue-i18n';
 
 export default {
   name: "Navbar",
   data() {
     return {
       menu: MenuJson,
-      isLanguage: true,
+      isLanguage: Boolean,
     };
   },
   created() {},
-  methods: {},
+  updated() {
+    this.changeLanguage();
+  },
+  methods: {
+    changeLanguage(){
+      const { locale } = useI18n();
+      if (this.isLanguage) {
+        localStorage.setItem('lang', 'en')
+      } else {
+        localStorage.setItem('lang', 'kh')
+      }
+      // Load language from localStorage on app start
+      locale.value = localStorage.getItem('lang');
+    }
+  },
+  mounted() {
+    if(localStorage.getItem('lang') === 'en' || localStorage.getItem('lang') === null) {
+      this.isLanguage = true;
+    }else {
+      this.isLanguage = false;
+    }
+  }
 };
--- src/main.js
+++ src/main.js
@@ -4,9 +4,21 @@ import router from "./router";
 import store from "./store/store";
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import { createI18n } from 'vue-i18n';
 
 import "./style.css";
 import "./assets/main.css"
+
+import kh from './utils/json/i18n/kh.json';
+import en from './utils/json/i18n/en.json';
+
+// Create i18n instance
+const i18n = createI18n({
+    legacy: false,  // Required for Vue 3 Composition API
+    locale: localStorage.getItem('lang') || 'en',   // Default locale
+    fallbackLocale: 'en', // Fallback locale
+    messages: { en, kh }
+});
 
 const app = createApp(App);
 
@@ -20,5 +32,6 @@ app.use(PrimeVue, {
          preset: Aura
      }
  });
+app.use(i18n);
 
 app.mount("#app");
