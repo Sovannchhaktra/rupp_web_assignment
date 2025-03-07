96278859eae121538c660a88a416771d819fd8ae
Sovannchhaktra <sovannchhaktra.nai@igtech.asia>
2025-03-07 16:33:52 +0700

    enhancement payment

--- src/views/addToCart/script.js
+++ src/views/addToCart/script.js
@@ -1,14 +1,30 @@
 
+import Dialog from 'primevue/dialog';
 import InputText from 'primevue/inputtext';
+import Panel from 'primevue/panel';
+import Card from 'primevue/card';
+import FloatLabel from "primevue/floatlabel";
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import DatePicker from 'primevue/datepicker';
+
 
 export default {
   name: "ListCart",
   data() {
     return {
       products: 4,
+      visible: false
     };
   },
   components: {
-    InputText
+    InputText,
+    Dialog,
+    Panel,
+    Card,
+    FloatLabel,
+    InputIcon,
+    IconField,
+    DatePicker
   }
 };
