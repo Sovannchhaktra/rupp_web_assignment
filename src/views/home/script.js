fa71c4edef4c2a1b039a31ae689c59c19afb3a65
Sovannchhaktra <sovannchhaktra.nai@igtech.asia>
2025-03-19 14:03:45 +0700

    add slider in home page

--- src/views/home/script.js
+++ src/views/home/script.js
@@ -1,5 +1,5 @@
 import Loader from "../../components/shared/loader/index.vue"
-
+import Galleria from 'primevue/galleria';
 export default {
   name: "Home",
   data() {
@@ -7,10 +7,43 @@ export default {
       star: 5,
       lists: 4,
       myData: "Hello world",
-      isLoading: false
+      isLoading: false,
+      images: [
+        {
+          itemImageSrc: 'https://placehold.co/100x35',
+          thumbnailImageSrc: 'https://placehold.co/60x60',
+          alt: 'Description for Image 1',
+          title: 'Title 1'
+        },
+        {
+          itemImageSrc: 'https://placehold.co/100x35',
+          thumbnailImageSrc: 'https://placehold.co/60x60',
+          alt: 'Description for Image 1',
+          title: 'Title 1'
+        },
+        {
+          itemImageSrc: 'https://placehold.co/100x35',
+          thumbnailImageSrc: 'https://placehold.co/60x60',
+          alt: 'Description for Image 1',
+          title: 'Title 1'
+        },
+        {
+          itemImageSrc: 'https://placehold.co/100x35',
+          thumbnailImageSrc: 'https://placehold.co/60x60',
+          alt: 'Description for Image 1',
+          title: 'Title 1'
+        },
+        {
+          itemImageSrc: 'https://placehold.co/100x35',
+          thumbnailImageSrc: 'https://placehold.co/60x60',
+          alt: 'Description for Image 1',
+          title: 'Title 1'
+        },
+      ]
     };
   },
   components: {
-    Loader
+    Loader,
+    Galleria
   },
 };
